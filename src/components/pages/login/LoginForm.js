f5f08958bbd35036996b961ae9ac9360ea2fcb09
reactif974 <patrick.perroudon@gmail.com>
2022-10-13 12:04:13 +0200

    🩹 F02: ORDER > Fix comments in PR

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -6,28 +6,29 @@ export default function LoginForm() {
     // state
     const [newName, setNewName] = useState("");
         // is authorized to access the order page
-        const [goToOrderPage, setGoToOrderPage] = useState(false);
+        const [isAuthorisedUser, setIsAuthorisedUser] = useState(false);
         // users account
         const users = [
             {username: 'bob'},
             {username: 'claire'}
         ];
-    const account = users.find((user) => user.username === newName);
+
+    const hasAccount = users.find((user) => user.username === newName);
     const navigate = useNavigate();
 
     // comportements
 
     // form submission
     const handleSubmit = (event) => {
+
         event.preventDefault();
-        // redirection order page
-        if(goToOrderPage) {
-            navigate(`/order/${newName}`);
-        } else {
-            // input clear
-            setNewName("");
+        // input clear
+        setNewName("");
+        navigate(`/order/${newName}`);
+        
+        // redirection loginPage if user no authorized
+        if(!isAuthorisedUser)
             navigate('/');
-        }
     }
 
     // registration of the new name from the input form
@@ -51,7 +52,7 @@ export default function LoginForm() {
             required 
         />
         <button 
-            onClick={() => !account ? setGoToOrderPage(false) : setGoToOrderPage(true)} 
+            onClick={() => hasAccount && setIsAuthorisedUser(true)}
             type="submit">
                 Accédez à votre espace
         </button>
