71ab75f8569c22ac4c0c1d03b94218cd7cfe066c
reactif974 <patrick.perroudon@gmail.com>
2022-10-14 10:26:11 +0200

    💄 F03: create assets and styles folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import './styles/App.css';
 import ErrorPage from './components/pages/ErrorPage';
 import LoginPage from './components/pages/login/LoginPage';
 import OrderPage from './components/pages/OrderPage';
