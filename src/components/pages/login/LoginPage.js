9085680f1962d0759fb329944bad71bb8f761d20
reactif974 <p.perroudon69@gmail.com>
2022-11-15 22:05:40 +0100

    :sparkles: create Logo component and add Logo to LoginPage

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,14 +1,12 @@
 import LoginForm from './LoginForm';
 import styled from 'styled-components';
+import Logo from '../reusable-ui/Logo';
 
 export default function LoginPage() {
   // affichage (render)
   return (
     <LoginPageStyled>
-      <h1 className='login-title'>
-        <span className='crazee-span'>crazee</span>
-        <span>burger</span>
-      </h1>
+      <Logo />
       <LoginForm />
     </LoginPageStyled>
   )
@@ -29,26 +27,5 @@ const LoginPageStyled = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-    .login-title {
-      margin-top:55px;
-      .crazee-span {
-          &::after {
-            content: "";
-            width: 280px;
-            height: 188px;
-            display: inline-block;
-            position: relative;
-            top: 60px;
-            left:13px;
-            background-image: url('images/logo-orange.png');
-            background-size: 243px 160px;
-            background-repeat: no-repeat;
-          }
-        }
-      > span{
-      color: #FF9A00;
-      font-size: 135px;
-      }
-    }
 `;
 
---
+++ src/components/pages/reusable-ui/Logo.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { theme } from '../../../theme';
+
+export default function Logo() {
+    // affichage
+    return (
+        <div>
+            <LogoStyled>
+                <span className='crazee-span'>crazee</span>
+                <span>burger</span>
+            </LogoStyled>
+        </div>
+    )
+}
+
+const LogoStyled = styled.h1`
+    margin-top:55px;
+    > span {
+        color: ${theme.colors.primary_burger};
+        font-size: ${parseInt(theme.fonts.P6, 10)*2}px;
+    }
+    .crazee-span {
+        &::after {
+          content: "";
+          width: 280px;
+          height: 188px;
+          display: inline-block;
+          position: relative;
+          top: 60px;
+          left:13px;
+          background-image: url('images/logo-orange.png');
+          background-size: 243px 160px;
+          background-repeat: no-repeat;
+        }
+    }
+`
