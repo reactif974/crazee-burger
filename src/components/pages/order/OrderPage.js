5c534f23e021d0798524372442cbeaae513cd22e
reactif974 <p.perroudon69@gmail.com>
2022-11-18 11:22:30 +0100

    :sparkles: OrderPage > add principal section with background white

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import NavBar from '../reusable-ui/NavBar';
 import styled from "styled-components";
+import { theme } from '../../../theme';
 
 export default function OrderPage() {
     // state
@@ -10,14 +11,34 @@ export default function OrderPage() {
     // affichage(render)
     return (
         <OrderPageStyled>
-            <NavBar userName={name} picsPath="../images/logo-orange.png"/>
+            <div className="section-container">
+                <NavBar userName={name} picsPath="../images/logo-orange.png"/>
+            </div>
         </OrderPageStyled>
     )
 }
 
 const OrderPageStyled = styled.div`
+    position:absolute;
+    width:100%;
     height:100vh;
     background: url("../images/logo-orange.png"), #FF9E1C;
     background-size: 80px;
-    // background-color: #FF9E1C;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    .section-container {
+        position:fixed;
+        top:0;
+        left:0;
+        right:0;
+        margin:110px auto 0 auto;
+        display:flex;
+        flex-direction:row;
+        width:95%;
+        height:85%;
+        background-color:${theme.colors.white};
+    }
+    
 `
