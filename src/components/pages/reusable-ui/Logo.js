00878818c6bcd172cee740a0d8e35e75c3efb3f8
reactif974 <p.perroudon69@gmail.com>
2022-11-18 12:10:37 +0100

    :ok_hand: create refreshPage in utils file

--- src/components/pages/reusable-ui/Logo.js
+++ src/components/pages/reusable-ui/Logo.js
@@ -1,57 +1,62 @@
 import styled from 'styled-components'
 import { theme } from '../../../theme'
 import logo from '../../../assets/logo-orange.png'
+import refreshPage from '../../../utils/refresh'
+
 
 
 export default function Logo() {
-    
+
     return (
-        <div onClick={() => window.location.reload(false)} style={{ cursor:"pointer"}}>
-            <LogoStyled>
+        <LogoStyled onClick={() => refreshPage() }>
+            <h1>
                 crazee 
                 <img src={logo} alt="logo-crazy-burger"/> 
                 burger
-            </LogoStyled>
-        </div>
+            </h1>
+        </LogoStyled>
     )
 }
 
-const LogoStyled = styled.h1`
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    justify-content: space-around;
-    font-size: 34px;
-    color: ${theme.colors.primary_burger};
-    font-family: 'Amatic SC', cursive;
-    margin-bottom: 0px;
-    transform: scale(1);
-    img {
-        padding: 0 ${theme.spacing.xs} 0 ${theme.spacing.xs};
-        height: 50px;
-    }
-    @media ${theme.devices.tablet} {
-        transform: scale(0.8);
-    }
-    @media ${theme.devices.mobileL} {
+const LogoStyled = styled.div`
+    cursor:pointer;
+    h1 {
         display: flex;
-        flex-direction: column;
+        flex-flow: row nowrap;
         align-items: center;
-        justify-content: center;
-        margin-top:-40px;
-    }
-    @media ${theme.devices.mobileM} {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin-top:-40px;
-    }
-    @media all and (min-width: 425px) and (max-width: 768px) {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin-top:-40px;
+        justify-content: space-around;
+        font-size: 34px;
+        color: ${theme.colors.primary_burger};
+        font-family: 'Amatic SC', cursive;
+        margin-bottom: 0px;
+        transform: scale(1);
+        img {
+            padding: 0 ${theme.spacing.xs} 0 ${theme.spacing.xs};
+            height: 50px;
+        }
+        @media ${theme.devices.tablet} {
+            transform: scale(0.8);
+        }
+        @media ${theme.devices.mobileL} {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top:-40px;
+        }
+        @media ${theme.devices.mobileM} {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top:-40px;
+        }
+        @media all and (min-width: 425px) and (max-width: 768px) {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top:-40px;
+        }
     }
 `
---
+++ src/utils/refresh.js
@@ -0,0 +1,5 @@
+const refreshPage = () => {
+    window.location.reload(false);
+}
+
+export default refreshPage;
