9adc57da1111237c3353bf5a28d6047653f0d21a
reactif974 <p.perroudon69@gmail.com>
2022-11-25 09:17:01 +0100

    :lipstick: OrderPage > NavBar > add border bottom

--- src/components/pages/reusable-ui/NavBar.js
+++ src/components/pages/reusable-ui/NavBar.js
@@ -28,6 +28,7 @@ const NavBarStyled = styled.nav`
     height:93px;
     background-color: ${theme.colors.white};
     padding: 0 ${theme.spacing.xxl} 0 ${theme.spacing.md};
+    border-bottom: 1px solid ${theme.colors.greyLight};
     border-radius: ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound} 0 0;
     h1 {
         margin-top: 0;
