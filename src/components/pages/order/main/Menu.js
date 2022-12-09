87d67165cef36c6c1a6b7f8c20249d7fe512ef4d
reactif974 <p.perroudon69@gmail.com>
2022-12-09 12:57:27 +0100

    :sparkles: FO6 > add ToggleButton in navBar + addapting UI

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -26,7 +26,7 @@ const MenuStyled = styled.div`
   grid-gap: 40px;
   justify-content: center;
   height: 80vh;
-  padding: 60px 0 80px 0;
+  padding: 60px 54px 80px 60px;
   border-bottom-left-radius: 0;
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   background-color: ${theme.colors.background_white};
--- src/components/pages/reusable-ui/NavBar.js
+++ src/components/pages/reusable-ui/NavBar.js
@@ -1,40 +1,41 @@
-import styled from 'styled-components'
-import { theme } from '../../../theme'
-import UserProfil from '../order/UserProfil'
-import Logo from './Logo'
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import UserProfil from "../order/UserProfil";
+import Logo from "./Logo";
+import ToggleButton from "./ToggleButton";
 
-export default function NavBar({userName}) {
-
-    return (
-        <NavBarStyled>
-            <Logo />
-            <div className='user-profile-right-side'>
-                <div className='button-admin'></div>
-                <UserProfil userName={userName}/>
-            </div>
-        </NavBarStyled>
-    )
+export default function NavBar({ userName }) {
+  return (
+    <NavBarStyled>
+      <Logo />
+      <div className="user-profile-right-side">
+        <ToggleButton />
+        <UserProfil userName={userName} />
+      </div>
+    </NavBarStyled>
+  );
 }
 
-
 const NavBarStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  width: 1400px;
+  max-width: 1400px;
+  height: 93px;
+  background-color: ${theme.colors.white};
+  padding: 0 ${theme.spacing.md} 0 ${theme.spacing.md};
+  border-bottom: 1px solid ${theme.colors.greyLight};
+  border-radius: ${theme.borderRadius.extraRound}
+    ${theme.borderRadius.extraRound} 0 0;
+  h1 {
+    margin-top: 0;
+  }
+  .user-profile-right-side {
     display: flex;
-    justify-content:center;
-    justify-content: space-between;
     align-items: center;
-    margin: 0 auto;
-    width:1400px;
-    max-width:1400px;
-    height:93px;
-    background-color: ${theme.colors.white};
-    padding: 0 ${theme.spacing.xxl} 0 ${theme.spacing.md};
-    border-bottom: 1px solid ${theme.colors.greyLight};
-    border-radius: ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound} 0 0;
-    h1 {
-        margin-top: 0;
-    }
-    .user-profile-right-side {
-        display: flex;
-        align-items: center;
-    }
-`
+    padding-right: 50px;
+    height: 100%;
+  }
+`;
