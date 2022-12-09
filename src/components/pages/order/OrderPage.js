958155245436ad832f8b450eedace4524e9aaa62
reactif974 <p.perroudon69@gmail.com>
2022-12-09 11:34:32 +0100

    :ok_hand: OrderPage > changing naming MenuCard component

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,48 +1,47 @@
-import { useParams } from 'react-router-dom'
-import NavBar from '../reusable-ui/NavBar'
-import styled from 'styled-components'
-import { theme } from '../../../theme'
-import logoOrange from '../../../assets/logo-orange.png'
-import MenuCard from './main/MenuCard'
+import { useParams } from "react-router-dom";
+import NavBar from "../reusable-ui/NavBar";
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import logoOrange from "../../../assets/logo-orange.png";
+import Menu from "./main/Menu";
 
 export default function OrderPage() {
+  const { name } = useParams();
 
-    const {name} = useParams();
-
-    return (
-        <OrderPageStyled>
-            <NavBar userName={name}/>
-            <main className="main-container">
-                {/* <div className="sideBar-left"></div> */}
-                <MenuCard />
-            </main>
-        </OrderPageStyled>
-    )
+  return (
+    <OrderPageStyled>
+      <NavBar userName={name} />
+      <main className="main-container">
+        {/* <div className="sideBar-left"></div> */}
+        <Menu />
+      </main>
+    </OrderPageStyled>
+  );
 }
 
 const OrderPageStyled = styled.div`
-    display: flex;
-    width:100%;
-    height:100vh;
-    flex-direction: column;
-    justify-content: center;
-    // background: url(${logoOrange}), #FF9E1C;
-    background: #FF9E1C;
-    background-size: 110px;
-    background-repeat: repeat;
-    background-position:cover;
-    .main-container {
-        display:grid;
-        /* grid-template-columns: 17% 1fr; */
-        grid-template-columns: 1fr auto;
-        width:1400px;
-        max-width: 1400px;
-        align-self: center;
-        .sideBar-left {
-            background-color: ${theme.colors.background_dark};
-            height:80vh;
-            width:100%;
-            border-bottom-left-radius: ${theme.borderRadius.extraRound};
-        }
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  // background: url(${logoOrange}), #FF9E1C;
+  background: #ff9e1c;
+  background-size: 110px;
+  background-repeat: repeat;
+  background-position: cover;
+  .main-container {
+    display: grid;
+    /* grid-template-columns: 17% 1fr; */
+    grid-template-columns: 1fr auto;
+    width: 1400px;
+    max-width: 1400px;
+    align-self: center;
+    .sideBar-left {
+      background-color: ${theme.colors.background_dark};
+      height: 80vh;
+      width: 100%;
+      border-bottom-left-radius: ${theme.borderRadius.extraRound};
     }
-`
+  }
+`;
