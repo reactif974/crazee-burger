25fc90709e265676ffee97e667a9271bda6e470e
reactif974 <p.perroudon69@gmail.com>
2022-12-13 16:43:24 +0100

    :sparkles: FO7 > PanelTabs > create tabs + style

--- src/components/pages/order/PanelTabs.js
+++ src/components/pages/order/PanelTabs.js
@@ -1,8 +1,43 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import Button from "../reusable-ui/Button";
+import { BsChevronDown } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+import { RiPencilFill } from "react-icons/ri";
 
 export default function PanelTabs() {
-  return <PanelTabsStyled>panel</PanelTabsStyled>;
+  return (
+    <PanelTabsStyled>
+      <div className="bloc-tabs">
+        <Button
+          text={""}
+          className={"tabs tabs-one"}
+          Icon={<BsChevronDown />}
+        />
+        <Button
+          text={"Ajouter un produit"}
+          className={"tabs tabs-two"}
+          Icon={<AiOutlinePlus />}
+        />
+        <Button
+          text={"Modifier un produit"}
+          className={"tabs tabs-three"}
+          Icon={<RiPencilFill />}
+        />
+      </div>
+      <div className="onglets-content">
+        <div className="contenu">
+          <h2>contenu 1</h2>
+        </div>
+        <div className="contenu">
+          <h2>contenu 2</h2>
+        </div>
+        <div className="contenu">
+          <h2>contenu 3</h2>
+        </div>
+      </div>
+    </PanelTabsStyled>
+  );
 }
 
 const PanelTabsStyled = styled.div`
@@ -13,4 +48,74 @@ const PanelTabsStyled = styled.div`
   bottom: 0;
   right: 0;
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  .bloc-tabs {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: 1fr;
+    background-color: transparent;
+    width: 100%;
+    margin-top: -35px;
+    width: 100%;
+    .tabs {
+      cursor: pointer;
+      svg {
+        padding-right: 5px;
+        font-size: ${theme.fonts.P1};
+      }
+    }
+    .tabs-one {
+      grid-column-start: 2;
+      grid-column-end: 3;
+    }
+    .tabs-two {
+      grid-column-start: 3;
+      grid-column-end: 5;
+    }
+    .tabs-three {
+      grid-column-start: 5;
+      grid-column-end: 7;
+    }
+    .active-tabs {
+      background: white;
+      border-bottom: 1px solid transparent;
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: -5px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: calc(100% + 2px);
+        height: 5px;
+        background: rgb(88, 147, 241);
+      }
+    }
+
+    .content-tabs {
+      flex-grow: 1;
+    }
+    .content {
+      background: white;
+      padding: 20px;
+      width: 100%;
+      height: 100%;
+      display: none;
+    }
+    .content h2 {
+      padding: 0px 0 5px 0px;
+    }
+    .content hr {
+      width: 100px;
+      height: 2px;
+      background: #222;
+      margin-bottom: 5px;
+    }
+    .content p {
+      width: 100%;
+      height: 100%;
+    }
+    .active-content {
+      display: block;
+    }
+  }
 `;
--- src/components/pages/reusable-ui/Button.js
+++ src/components/pages/reusable-ui/Button.js
@@ -1,34 +1,44 @@
-import styled from 'styled-components'
-import { theme } from '../../../theme'
+import styled from "styled-components";
+import { theme } from "../../../theme";
 
-export default function Button({text,type,Icon,className}) {
-    return (
-        <ButtonStyled>
-            <button type={type} className={className}>
-                {text}
-                {Icon && Icon}
-            </button>
-        </ButtonStyled>
-    )
+export default function Button({ text, type, Icon, className }) {
+  return (
+    <ButtonStyled type={type} className={className}>
+      {className !== "button_submit" ? Icon && Icon : null}
+      {text}
+      {className === "button_submit" ? Icon && Icon : null}
+    </ButtonStyled>
+  );
 }
 
-const ButtonStyled = styled.div`
-    .button_submit {
-        width: 100%;
-        height: 65px;
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: center;
-        align-items: center;
-        border:none;
-        border-radius: ${theme.borderRadius.round};
-        background-color: ${theme.colors.background_button_submit};
-        color: ${theme.colors.white};
-        font-family: 'Open Sans', sans-serif;
-        font-size: ${theme.fonts.P1};
-        font-weight: ${theme.weights.bold};
-        &:hover {
-            cursor:pointer;
-        }
-    }
-`
+const ButtonStyled = styled.button`
+  width: 100%;
+  height: ${(props) => (props.className === "button_submit" ? "65px" : "35px")};
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  border: ${(props) =>
+    props.className === "button_submit"
+      ? "none"
+      : `0.6px solid ${theme.colors.greyDark}`};
+  border-radius: ${(props) =>
+    props.className === "button_submit"
+      ? theme.borderRadius.round
+      : `${theme.borderRadius.round} ${theme.borderRadius.round} 0 0`};
+  background-color: ${(props) =>
+    props.className === "button_submit"
+      ? theme.colors.background_button_submit
+      : theme.colors.background_white};
+  color: ${(props) =>
+    props.className === "button_submit"
+      ? theme.colors.white
+      : theme.colors.background_dark};
+  font-family: "Open Sans", sans-serif;
+  font-size: ${(props) =>
+    props.className === "button_submit" ? theme.fonts.P1 : theme.fonts.XS};
+  font-weight: ${theme.weights.bold};
+  &:hover {
+    cursor: pointer;
+  }
+`;
