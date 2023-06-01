2fb8c7892f4f8a9c321eec056ab03f36841f34b2
reactif974 <p.perroudon69@gmail.com>
2023-06-01 10:34:44 +0400

    :sparkles: F13 > create firebase config file

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfQWu7x5Sf2_lHD-a5iGCTMjjJBVjO1ug",
+  authDomain: "crazee-burger-auth.firebaseapp.com",
+  projectId: "crazee-burger-auth",
+  storageBucket: "crazee-burger-auth.appspot.com",
+  messagingSenderId: "999406494265",
+  appId: "1:999406494265:web:8a5d13ef356826cff663e8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
