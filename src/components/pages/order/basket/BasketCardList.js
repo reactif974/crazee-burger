f9c820b59a938ec9057a3e2ecf9babad4a145a49
reactif974 <p.perroudon69@gmail.com>
2023-06-29 22:06:22 +0400

    :sparkles: F14 > Basket > animate basket card - add and delete

--- src/components/pages/order/basket/BasketCardList.js
+++ src/components/pages/order/basket/BasketCardList.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import GlobalContext from "../../../../context/GlobalContext";
 import BasketCard from "./BasketCard";
 import { findInArray } from "../../../../utils/array/array";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function BasketCardList() {
   const {
@@ -24,27 +25,83 @@ export default function BasketCardList() {
     handleProductSelected(idProductClicked);
   };
   return (
-    <BasketCardListStyled>
+    <TransitionGroup
+      component={BasketCardListStyled}
+      className={"transition-group"}
+    >
       {basket.map((product) => {
         const menuProduct = findInArray(product.id, menu);
         return (
-          <BasketCard
+          <CSSTransition
+            appear={true}
+            classNames={"product-card"}
             key={menuProduct.id}
-            imageSource={menuProduct.imageSource}
-            title={menuProduct.title}
-            price={menuProduct.price}
-            quantity={product.quantity}
-            productId={menuProduct.id}
-            variant={productSelected.id === menuProduct.id ? "selected" : ""}
-            onDelete={(event) => handleDeleteProductCard(event, menuProduct.id)}
-            onClick={(event) => handleOnClick(event, menuProduct.id)}
-          />
+            timeout={500}
+          >
+            <BasketCard
+              imageSource={menuProduct.imageSource}
+              title={menuProduct.title}
+              price={menuProduct.price}
+              quantity={product.quantity}
+              productId={menuProduct.id}
+              variant={productSelected.id === menuProduct.id ? "selected" : ""}
+              onDelete={(event) =>
+                handleDeleteProductCard(event, menuProduct.id)
+              }
+              onClick={(event) => handleOnClick(event, menuProduct.id)}
+            />
+          </CSSTransition>
         );
       })}
-    </BasketCardListStyled>
+    </TransitionGroup>
   );
 }
 
 const BasketCardListStyled = styled.div`
   padding-top: 10px;
+
+  // phase de mounting du composant : cible le 1er element de la liste
+  .product-card-appear {
+    .card {
+      transform: translateX(100px);
+      opacity: 0%;
+    }
+  }
+  .product-card-appear-active {
+    .card {
+      transform: translateX(0px);
+      opacity: 100%;
+      transition: 0.5s;
+    }
+  }
+
+  // phase de mounting du composant
+  .product-card-enter {
+    .card {
+      transform: translateX(100px);
+      opacity: 0%;
+    }
+  }
+  .product-card-enter-active {
+    .card {
+      transform: translateX(0px);
+      opacity: 100%;
+      transition: 0.5s;
+    }
+  }
+
+  // phase de unMounting du composant
+  .product-card-exit {
+    .card {
+      transform: translateX(0px);
+      opacity: 100%;
+    }
+  }
+  .product-card-exit-active {
+    .card {
+      transform: translateX(-100px);
+      opacity: 0%;
+      transition: 0.5s;
+    }
+  }
 `;
