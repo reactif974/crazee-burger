ce6de17aeffe37e7334bd32afe09232042f3eafd
reactif974 <p.perroudon69@gmail.com>
2023-06-30 09:12:06 +0400

    :construction: F14 : Basket > create CasinoEffect() and make both elements vivible on props change

--- src/components/pages/reusable-ui/CasinoEffect.js
+++ src/components/pages/reusable-ui/CasinoEffect.js
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function CasinoEffect({ className, count }) {
-  return <CasinoEffectStyled className={className}>{count}</CasinoEffectStyled>;
+  return (
+    <TransitionGroup component={CasinoEffectStyled}>
+      <CSSTransition classNames={"count-animated"} timeout={5000} key={count}>
+        <span className={className}>{count}</span>
+      </CSSTransition>
+    </TransitionGroup>
+  );
 }
 
 const CasinoEffectStyled = styled.div`
@@ -12,4 +19,23 @@ const CasinoEffectStyled = styled.div`
   font-size: ${theme.fonts.P4};
   line-height: 45px;
   color: ${theme.colors.primary};
+
+  .count-animated {
+    background-color: red;
+  }
+  .count-animated-active {
+    background-color: blue;
+    transition: 2s;
+  }
+  .count-animated-done {
+    background-color: green;
+  }
+
+  .count-animated-exit {
+    background-color: yellow;
+  }
+  .count-animated-exit-active {
+    background-color: black;
+    transition: 2s;
+  }
 `;
