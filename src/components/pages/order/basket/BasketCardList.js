6e0d0673cbf7e07cc1614ed30cf377f159662cb9
reactif974 <p.perroudon69@gmail.com>
2023-07-06 14:49:34 +0400

    :sparkles: F15 : PUBLICITY > Basket > BasKetCardList > add badge in card if product is publicised

--- src/components/pages/order/basket/BasketCardList.js
+++ src/components/pages/order/basket/BasketCardList.js
@@ -8,6 +8,7 @@ import { basketCardsAnimation } from "../../../../theme/animations";
 import { convertStringToBoolean } from "../../../../utils/string/string";
 import { formatPrice } from "../../../../utils/number/format";
 import { BASKET_MESSAGES } from "../../../../enums/messages";
+import Sticker from "../../reusable-ui/Sticker";
 
 export default function BasketCardList() {
   const {
@@ -42,22 +43,29 @@ export default function BasketCardList() {
             key={menuProduct.id}
             timeout={500}
           >
-            <BasketCard
-              imageSource={menuProduct.imageSource}
-              title={menuProduct.title}
-              price={
-                convertStringToBoolean(menuProduct.isAvailable)
-                  ? formatPrice(menuProduct.price)
-                  : BASKET_MESSAGES.NOT_AVAILABLE
-              }
-              quantity={product.quantity}
-              productId={menuProduct.id}
-              variant={productSelected.id === menuProduct.id ? "selected" : ""}
-              onDelete={(event) =>
-                handleDeleteProductCard(event, menuProduct.id)
-              }
-              onClick={(event) => handleOnClick(event, menuProduct.id)}
-            />
+            <div className="card-container">
+              {convertStringToBoolean(menuProduct.isPublicised) && (
+                <Sticker className="badge-new" />
+              )}
+              <BasketCard
+                imageSource={menuProduct.imageSource}
+                title={menuProduct.title}
+                price={
+                  convertStringToBoolean(menuProduct.isAvailable)
+                    ? formatPrice(menuProduct.price)
+                    : BASKET_MESSAGES.NOT_AVAILABLE
+                }
+                quantity={product.quantity}
+                productId={menuProduct.id}
+                variant={
+                  productSelected.id === menuProduct.id ? "selected" : ""
+                }
+                onDelete={(event) =>
+                  handleDeleteProductCard(event, menuProduct.id)
+                }
+                onClick={(event) => handleOnClick(event, menuProduct.id)}
+              />
+            </div>
           </CSSTransition>
         );
       })}
@@ -68,4 +76,25 @@ export default function BasketCardList() {
 const BasketCardListStyled = styled.div`
   padding-top: 10px;
   ${basketCardsAnimation}
+  .card-container {
+    margin: 10px 16px;
+    height: 86px;
+    box-sizing: border-box;
+    position: relative;
+    :first-child {
+      margin-top: 20px;
+    }
+    :last-child {
+      margin-bottom: 20px;
+    }
+
+    .badge-new {
+      position: absolute;
+      z-index: 1;
+      bottom: 10%;
+      left: 21%;
+      transform: translateY(-21%);
+      transform: translateX(-5%);
+    }
+  }
 `;
